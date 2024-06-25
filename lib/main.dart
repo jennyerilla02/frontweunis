04336078147dfb6364c508eb5e44a8e1e30a2107
jennyerilla02 <erillajenny16@gmail.com>
2024-06-25 10:18:01 +0800

    Activity 1

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,62 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Center(
-          child: Text('UNIS'),
-        ),
+        title: Text('User Profile'),
         backgroundColor: Colors.pinkAccent,
+        centerTitle: true,
       ),
-      body: Center(
-        child: Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG91kngIUypoh2VaLcWUXt7LaoC0gyS4qS_w&s'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.pinkAccent,
-        child: Icon(
-            Icons.add,
-          color: Colors.black,
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'NAME',
+              style: TextStyle(
+                fontSize: 20.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+            Text(
+              'Jenny M. Erilla',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25.0,
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Text(
+              'YEAR',
+              style: TextStyle(
+                fontSize: 20.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+            Text(
+              '4th',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25.0,
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Text(
+              'EMAIL',
+              style: TextStyle(
+                fontSize: 20.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+            Text(
+              'erillajenny16@gmail.com',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25.0,
+              ),
+            ),
+          ],
         ),
       ),
     ),
   ));
 }
-
