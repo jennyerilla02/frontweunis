901fceed41bd3334d8e88ef96ac6ab14850f5929
jennyerilla02 <jennyerilla16@gmail.com>
2024-06-28 10:16:45 +0800

    final activity 2

--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:unis/services/product.dart';
+import 'package:unis/services/menuCard.dart';
 
 class Menu extends StatefulWidget {
   const Menu({super.key});
@@ -8,10 +10,39 @@ class Menu extends StatefulWidget {
 }
 
 class _MenuState extends State<Menu> {
+  List products = <Product>[
+    Product(productName: "Dark Choco", price: 49.99), //Product
+    Product(productName: "Okinawa", price: 45.99), //Product
+    Product(productName: "Red Velvet", price: 45.99), // Product
+    Product(productName: "Matcha", price: 49.99),
+    Product(productName: "Wintermelon", price: 49.99),
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('This is menu'),
+      appBar: AppBar(
+        backgroundColor: Colors.pinkAccent,
+        foregroundColor: Colors.white70,
+        title: Text(
+          'Menu',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 25.0,
+            color: Colors.black,
+
+          ),
+        ),
+        centerTitle: true,
+
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: products.map((product) => Menucard(product: product)).toList(),
+        ),
+      ),
     );
   }
 }
---
+++ lib/services/menuCard.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:unis/services/product.dart';
+
+class Menucard extends StatelessWidget {
+  final Product product;
+
+  Menucard({required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 500,
+      height: 100,
+      child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  product.productName,
+                  style: const TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),
+                ),
+                Text(
+                  "${product.price}",
+                  style: const TextStyle(color: Colors.black),
+                ),
+              ],
+            ),
+          )
+      ),
+    );
+  }
+}
